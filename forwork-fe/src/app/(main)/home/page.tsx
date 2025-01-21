0a3dc0917955fcd4f87413496ef89539b0d00112
유림 <yulimh001@gmail.com>
2025-01-22 03:23:44 +0900

    style: 홈 서비스 소개 #1

--- forwork-fe/src/app/(main)/home/page.tsx
+++ forwork-fe/src/app/(main)/home/page.tsx
@@ -3,6 +3,20 @@ const HomePage = () => {
         <div>
             <main>
                 <NavBar/>
+                <div className={"h-[158px]"}/>
+                {/* 서비스 소개*/}
+                <section className={"flex flex-col justify-center items-center gap-y-6 px-[264px] mt-[32px] mb-[56px]"}>
+                    <div className={"flex gap-x-[7px] p-3 rounded-[16px] bg-gray1 subtitle-md w-fit items-center"}>
+                        <Image src={"/image 55.png"} width={33} height={33} alt={"/image 55.png"}/>
+                        <span>이력서와 포트폴리오를 등록하고 <span className={"text-[#1c6cff]"}>시급 UP!</span></span>
+                        <div className={"flex items-center w-[20px] h-[20px]"}>
+                            <ArrowForwardIcon/>
+                        </div>
+                    </div>
+                    <div className={"flex justify-center items-center bg-gray3 rounded-[16px] w-full h-[160px]"}>서비스 소개
+                        배너
+                    </div>
+                </section>
 
                 <Footer />
             </main>
---
+++ forwork-fe/src/assets/common/ArrowForwardIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const ArrowForwardIcon = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={9}
+        height={16}
+        fill="none"
+        {...props}
+    >
+        <path
+            fill="#6F717C"
+            d="M6.852 7.86.825 1.833a.78.78 0 0 1-.232-.56.75.75 0 0 1 .224-.56.76.76 0 0 1 .56-.228q.332 0 .56.228L8.15 6.918q.206.205.293.444.09.24.089.498 0 .26-.089.499-.087.239-.293.444l-6.205 6.205a.78.78 0 0 1-.564.231.75.75 0 0 1-.564-.223.76.76 0 0 1-.228-.56q0-.333.228-.56z"
+        />
+    </svg>
+);
+export default ArrowForwardIcon;
