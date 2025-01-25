6640499c4b08b8e0ff3d7e9a65ff9bfc339eec34
유림 <yulimh001@gmail.com>
2025-01-25 21:07:01 +0900

    feat: 업직종 컴포넌트 생성#3

---
+++ forwork-fe/src/components/registration/JobCategory.tsx
@@ -0,0 +1,30 @@
+import Button from "@/src/components/common/Button";
+import JobCategoryItem from "@/src/components/registration/JobCategoryItem";
+import {jobCategoryList} from "@/src/utils/common";
+
+interface Props {
+
+}
+
+const JobCategory = (props: Props) => {
+    const {} = props;
+
+    return (
+        <div className={"flex flex-col gap-y-3"}>
+            {/* title */}
+            <div className={"title-md"}>업직종<span className={"text-main"}>*</span></div>
+            <div className={"grid grid-cols-7 gap-3"}>
+                {jobCategoryList.map((jobCategory) => {
+                    return (
+                        <JobCategoryItem>{jobCategory}</JobCategoryItem>
+                    )
+                })}
+            </div>
+
+
+            {/* 업직종 items */}
+
+        </div>
+    )
+}
+export default JobCategory;
