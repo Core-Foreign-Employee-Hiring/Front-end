6601857ce572793cb9d399093ab336bafc8eb97d
HyeEun_Lee <blackhaze1024@gmail.com>
2025-01-28 00:18:35 +0900

    feat:알바 후기 페이지 퍼블리싱 #7

--- forwork-fe/src/app/(main)/review/page.tsx
+++ forwork-fe/src/app/(main)/review/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import NavBar from "@/src/components/common/NavBar";
+import JobReviewBoard from "@/src/components/review/JobReviewBoard";
 
 const ReviewPage = () => {
-    return (
-        <div>
-            <main>
-                <NavBar/>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div>
+      <main>
+        <NavBar />
+        <div className="h-60" />
+        <JobReviewBoard />
+      </main>
+    </div>
+  );
+};
 export default ReviewPage;
